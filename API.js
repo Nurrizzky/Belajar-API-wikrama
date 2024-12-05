58f2a997000c041fde72b0f6ef9c0e4e47eb1110
Nurrizzky <nurrizkyr90@gmail.com>
2024-12-05 16:23:46 +0700

    First Commit

---
+++ API.js
@@ -0,0 +1,24 @@
+// Rest API === Menggunakan semua method / Pembuatan
+// Restfull API === Consume API nya / Pemakaian
+
+// PHP bikin API nya memakai :  Lumen, juga laravel
+// Javascript  :  Express, Node.js
+
+// HTTP status Code : 200, 400, 404, 500, 419
+// 401  === butuh token (token auth)
+
+// jwt, sangtum  pakacge == mnghasilkan token -> di simpat ke session 
+
+// simpan token di postman itu di headers, atau auth
+
+// Query params di Postman harus diisi di bagian params 
+
+// Saat diberikan API oleh orang lain Kita harus membaca documentasinya dari backend
+
+// Payload itu form di FE
+
+// FORM-data biasanya untuk PHP
+// RAW digunakan untuk js Sepengalaman
+
+// Body Buat payload / form kalo di bahasa FE
+
